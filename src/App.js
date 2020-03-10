9465e610a63a559a2f739552e973e55463c9a81f
Christopher Floyd <cfloyd252@gmail.com>
2020-03-10 13:23:47 -0700

    added state to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Route } from 'react-router-dom';
 import TabNav from './Components/TabNav/TabNav'
 import Overview from './Components/Overview/Overview'
 import LogView from './Components/LogView/LogView'
+import dummyStore from '../dummy-store'
 import './App.css'
 
-function App() {
-  return (
-    <main className='App'>
-      <Route exact path={'/'} component={Overview} />
-      <Route exact path={'/weight-log'} render={(routerProps) => {
-        return <LogView title='Weight Log' {...routerProps}/>
+class App extends Component {
+  state = {
+    weightData: [],
+    waterData: [],
+    activityData: [],
+  }
+
+  componentDidMount() {
+    this.setState(dummyStore)
+  }
+
+  render() {
+    return (
+      <main className='App'>
+        <Route exact path={'/'} component={Overview} />
+        <Route exact path={'/weight-log'} render={(routerProps) => {
+          return <LogView title='Weight Log' dataArray={this.state.weightData} {...routerProps}/>
+          }} />
+        <Route exact path={'/water-log'} render={(routerProps) => {
+          return <LogView title='Water Log' dataArray={this.state.waterData} {...routerProps}/>
         }} />
-      <Route exact path={'/water-log'} render={(routerProps) => {
-        return <LogView title='Water Log' {...routerProps}/>
-      }} />
-      <Route exact path={'/activity-log'} render={(routerProps) => {
-        return <LogView title='Activty Log' {...routerProps}/>
-      }} />
-      <Route path={'/'} component={TabNav} />
-    </main>
-  );
+        <Route exact path={'/activity-log'} render={(routerProps) => {
+          return <LogView title='Activty Log' dataArray={this.state.activityData} {...routerProps}/>
+        }} />
+        <Route path={'/'} component={TabNav} />
+      </main>
+    )
+  }
 }
 
 export default App;
--- src/Components/LogView/LogView.js
+++ src/Components/LogView/LogView.js
@@ -4,12 +4,19 @@ import DateContainer from '../DateContainer/DateContainer'
 
 export class LogView extends Component {
   render() {
+    const sortedArrayByDate = dataArray.sort(dataObject => {
+      return new
+    })
+
+    const dateContainers = sortedArrayByDate.map(dataObject => {
+      return <DateContainer date={dataObject.datetime} />
+    })
+
     return (
       <section className='Log-View'>
         <h1>{this.props.title}</h1>
         <button className="add-data">Add Weight</button>
-        <DateContainer />
-        <DateContainer />
+        {dateContainers}
       </section>
     )
   }
