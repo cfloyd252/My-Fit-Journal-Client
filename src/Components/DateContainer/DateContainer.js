bc33f737cf432672a2744668a5c1e4eba863fdc0
Christopher Floyd <cfloyd252@gmail.com>
2020-03-11 11:36:42 -0700

    complete array methods to properly display LogData component

--- src/Components/DateContainer/DateContainer.js
+++ src/Components/DateContainer/DateContainer.js
@@ -4,7 +4,10 @@ import LogData from '../LogData/LogData'
 
 export class DateContainer extends Component {
   render() {
-    const filteredArrayByDate = this.props.filter()
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    const filteredArrayByDate = this.props.dataArray.filter(dataObject => {
+      return this.props.date === new Date(dataObject.datetime).toLocaleDateString('en-US', options)
+    })
 
     const logData = filteredArrayByDate.map(dataObject => {
       return <LogData />
