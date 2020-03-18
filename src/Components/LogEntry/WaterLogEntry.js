97bd33bbc74daf1dc60610d8ae8466d569235b4e
Christopher Floyd <cfloyd252@gmail.com>
2020-03-18 11:32:29 -0700

    added entry form files

---
+++ src/Components/LogEntry/WaterLogEntry.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class WaterLogEntry extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default WaterLogEntry
