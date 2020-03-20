0b7ec7cf1ff9f838e5074ac3f9fc600e82154794
Christopher Floyd <cfloyd252@gmail.com>
2020-03-20 15:26:10 -0700

    changed config for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://murmuring-hamlet-60178.herokuapp.com//api',
+  API_ENDPOINT: 'https://murmuring-hamlet-60178.herokuapp.com/api',
   TOKEN_KEY: 'my-fit-journal-client-auth-token',
 }
