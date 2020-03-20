e80cc51f9ea4682b498ecbbb2977271ad5d3c588
Christopher Floyd <cfloyd252@gmail.com>
2020-03-20 08:50:15 -0700

    working add data button for activities

--- src/Components/LogEntryForm/ActivityLogEntry.js
+++ src/Components/LogEntryForm/ActivityLogEntry.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import './LogEntry.css';
+import EntriesApiService from "../../services/entries-api-service";
 
 export class ActivityLogEntry extends Component {
+  handleSubmit = ev => {
+    ev.preventDefault()
+    const { name_of_activity, start_time, end_time, calories} = ev.target
+    const startTimeValue = `${start_time.value}:00Z`
+    const endTimeValue = `${end_time.value}:00Z`
+    
+    EntriesApiService.postActivityEntry(name_of_activity.value, startTimeValue, endTimeValue, calories.value, 1)
+
+    this.props.history.push('/journal/activity')
+  }
   render() {
     return (
       <section className='entry_section'>
-        <form className='entry_form' id='activity_form'>
+        <form className='entry_form' id='activity_form' onSubmit={this.handleSubmit}>
           <label htmlFor='name_of_activity'>Name of activity</label>
           <input name='name_of_activity' type='text' required />
           <label for="start_time">Start time</label>
