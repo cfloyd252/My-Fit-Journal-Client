36f9e65684527380c0c26dc7596c85caab503187
Christopher Floyd <58857442+cfloyd252@users.noreply.github.com>
2020-05-13 12:15:51 -0700

    working on reg

--- src/services/users-api-service.js
+++ src/services/users-api-service.js
@@ -1,22 +1,20 @@
 import config from '../config'
 
-const AuthApiService = {
-  postLogin(credentials) {
-    return fetch(`${config.API_ENDPOINT}/auth/login`, {
+const UsersApiService = {
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
       },
-      body: JSON.stringify(credentials)
+      body: JSON.stringify(user),
     })
-      .then(res => 
+      .then(res =>
         (!res.ok)
           ? res.json().then(e => Promise.reject(e))
           : res.json()
-        )
+      )
   },
-
-  
 }
 
-export default AuthApiService
+export default UsersApiService
