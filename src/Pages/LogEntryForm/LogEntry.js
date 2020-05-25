30e634b4baee1a052f6e9331081f81d6a1676ae1
cfloyd252 <cfloyd252@gmail.com>
2020-05-25 14:38:07 -0700

    handle submit works for all log entries

--- src/Pages/LogEntryForm/LogEntry.js
+++ src/Pages/LogEntryForm/LogEntry.js
@@ -10,7 +10,7 @@ export class LogEntry extends Component {
   state = {
     logType: this.props.match.params.logType,
     exerciseName: {
-      value: '',
+      value: null,
       touched: false
     },
     quanity: {
@@ -26,7 +26,7 @@ export class LogEntry extends Component {
       touched: false
     },
     endTime: {
-      value: '',
+      value: null,
       touched: false
     },
     calories: {
@@ -51,9 +51,9 @@ export class LogEntry extends Component {
 
     console.log(newEntry)
     
-    // EntriesApiService.postEntry(newEntry)
-    //   .then(entry => console.log(entry))
-    //   .catch(error => this.context.setError(error));
+    EntriesApiService.postEntry(newEntry)
+      .then(entry => console.log(entry))
+      .catch(error => this.context.setError(error));
 
     this.props.history.push(`/journal/${this.state.logType}`)
   }
@@ -76,7 +76,7 @@ export class LogEntry extends Component {
                   <label htmlFor='quanity'>Quanity</label>
                   <input name='quanity' type='number' min='1' onChange={(e) => this.updateValue('quanity', e.target.value)}/>
                   <label htmlFor='unit_of_measurement'>Unit of Measurement</label>
-                  <select name='unit_of_measurement'onChange={(e) => this.updateValue('unitOfMeasurement', e.target.value)} >
+                  <select name='unit_of_measurement' onChange={(e) => this.updateValue('unitOfMeasurement', e.target.value)} >
                     <option value=''>Select</option>
                     <option value='ml'>ml</option>
                     <option value='cup'>cup</option>
@@ -93,14 +93,15 @@ export class LogEntry extends Component {
                 <section className='entry_section'>
                   <form className='entry_form' id='weight_form' onSubmit={this.handleSubmit}>
                   <label htmlFor='quanity'>Quanity</label>
-                  <input name='quanity' type='number' min='1'  />
+                  <input name='quanity' type='number' min='1' onChange={(e) => this.updateValue('quanity', e.target.value)} />
                   <label htmlFor='unit_of_measurement'>Unit of Measurement</label>
-                  <select name='unit_of_measurement'>
+                  <select name='unit_of_measurement' onChange={(e) => this.updateValue('unitOfMeasurement', e.target.value)}>
+                    <option>Select</option>
                     <option value='lb'>lbs</option>
                     <option value='kg'>kg</option>
                   </select>
                   <label htmlFor="start_time">Date and time</label>
-                  <input type="datetime-local" name="start_time" ></input>
+                  <input type="datetime-local" name="start_time" onChange={(e) => this.updateValue('startTime', e.target.value)} />
                   <button className ='submit_data' type="submit">Submit</button>
                 </form>
                 </section>
@@ -110,13 +111,13 @@ export class LogEntry extends Component {
                 <section className='entry_section'>
                   <form className='entry_form' id='exercise_form' onSubmit={this.handleSubmit}>
                     <label htmlFor='name_of_exercise'>Name of exercise</label>
-                    <input name='name_of_exercise' type='text' />
+                    <input name='name_of_exercise' type='text' onChange={(e) => this.updateValue('exerciseName', e.target.value)}/>
                     <label for="start_time">Start time</label>
-                    <input type="datetime-local" name="start_time"></input>
+                    <input type="datetime-local" name="start_time" onChange={(e) => this.updateValue('startTime', e.target.value)}/>
                     <label for="end_time">End time</label>
-                    <input type="datetime-local" name="end_time"></input>
+                    <input type="datetime-local" name="end_time" onChange={(e) => this.updateValue('endTime', e.target.value)}/>
                     <label htmlFor='calories'>Calories burned</label>
-                    <input name='calories' type='number' min='1'></input>
+                    <input name='calories' type='number' min='1' onChange={(e) => this.updateValue('calories', e.target.value)}/>
                     <button className ='submit_data' type="submit">Submit</button>
                   </form>
                 </section>
