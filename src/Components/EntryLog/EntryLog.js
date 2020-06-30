c70750564371c438772761f3c0cf47a7a25b3a94
cfloyd252 <cfloyd252@gmail.com>
2020-06-30 02:43:27 -0700

    working click console logs

--- src/Components/EntryLog/EntryLog.js
+++ src/Components/EntryLog/EntryLog.js
@@ -10,33 +10,18 @@ class EntryLog extends Component {
   onSingleClickHandler = () => {
     this.timer = setTimeout(() => {
       if (!this.prevent) {
-        // this.setState(
-        //   {
-        //     displayText: "Single Click"
-        //   },
-        //   () => {
-        //     console.log(this.state);
-        //   }
-        // );
-        console.log('hello')
+        console.log('single click')
       }
     }, this.delay);
   };
+  
   onDoubleClickHandler = () => {
-    // clearTimeout(this.timer);
-    // this.prevent = true;
-    // this.setState(
-    //   {
-    //     displayText: "Double Click"
-    //   },
-    //   () => {
-    //     console.log(this.state);
-    //     setTimeout(() => {
-    //       this.prevent = false;
-    //     }, this.delay);
-    //   }
-    // );
-    console.log('hello 2 clicks')
+    clearTimeout(this.timer);
+    this.prevent = true;
+    console.log('double click');
+    setTimeout(() => {
+      this.prevent = false;
+    }, this.delay);
   };
 
   render() {
@@ -47,11 +32,8 @@ class EntryLog extends Component {
       return (
         <div
           className="log-data"
-          onClick={() => console.log('single click')}
-          onMouseEnter={() => console.log('hello')}
-          onMouseLeave={() => console.log('bye')}
-          onDoubleClick={() => alert('double click')}
-          // onTouchStart={this.onDoubleClickHandler}
+          onClick={this.onSingleClickHandler}
+          onDoubleClick={this.onDoubleClickHandler}
         >
           <p>{this.props.entryInfo.quanity} {this.props.entryInfo.unit_of_measurement}</p>
         </div>
